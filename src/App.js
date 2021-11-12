00a69cd966db3d23d81e7652151e7ab97316d1b7
Ivan <ivanveselinov22@gmail.com>
2021-11-12 14:12:43 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-function App() {
-  return <div className="bg-black h-[300px] flex">hello unique</div>;
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="bg-red-400 w-[980px] h-screen ">
+      <div className="container bg-yellow-200 text-center m-auto min-h-0">asdaasdsadsadasds</div>
+      <div className="bg-yellow-200 min-h-[200px]"></div>
+    </div>
+    
+  )
 }
 
-export default App;
+export default App
