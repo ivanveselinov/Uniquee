0a37994d7233a3cfbecffbe9b42dbdbf645682f9
EhsanAlimo <ehsan.russell69@gmail.com>
2021-11-14 10:03:20 +1100

    left sidebar

--- src/components/sidebarLeft/SidebarLeft.js
+++ src/components/sidebarLeft/SidebarLeft.js
@@ -1,7 +1,12 @@
 import React from "react";
+import CheckboxList from "./CheckBoxList";
 
 function SidebarLeft() {
-  return <div className=" w-1/4 border-4 text-left bg-indigo-100 ">this is the left sidebar</div>;
+  return (
+    <div className=" w-1/4 border-4 text-left bg-indigo-100 ">
+      <CheckboxList />
+    </div>
+  );
 }
 
 export default SidebarLeft;
