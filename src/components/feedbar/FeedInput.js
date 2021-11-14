d06cf5d8d81f9f697ffac8cc29f8f849caf4d27d
EhsanAlimo <ehsan.russell69@gmail.com>
2021-11-14 14:15:24 +1100

    feedinput Ref

--- src/components/feedbar/FeedInput.js
+++ src/components/feedbar/FeedInput.js
@@ -1,22 +1,34 @@
-import React from "react";
-
+import React, { useRef } from "react";
+import { useContextProvider } from "../../context/StateProvider";
 function FeedInput() {
+  const [{ user }, dispatch] = useContextProvider();
+  const descriptionRef = useRef(null);
+  const priceRef = useRef(null);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log(descriptionRef.current.value);
+    console.log(priceRef.current.value);
+  };
   return (
     <div className="mx-auto w-3/4 h-1/4 border-2 mt-10 p-3 rounded-lg">
-      <form action="" className="w-full ">
+      <form className="w-full " onSubmit={submitHandler}>
         <div className="flex space-x-2">
           <img
             className="w-8 h-8 rounded-full ring-1"
-            src="https://thethirdwave.co/app/themes/ttw/assets/img/third-wave-logo-color.svg"
+            src={user?.photoURL}
             alt=""
           />
           <div className="flex-grow">
             <input
+              ref={descriptionRef}
               type="text"
               placeholder="Tell us about your product"
               className="rounded-full h-10 placeholder-gray-400 bg-gray-100 flex-grow w-full"
             />
-            <button className="hidden">Submit</button>
+            <button className="hidden" onClick={submitHandler}>
+              Submit
+            </button>
           </div>
         </div>
       </form>
@@ -25,12 +37,12 @@ function FeedInput() {
           <p>Photo/Video</p>
         </div>
         <div className="inputBtn">
-          <p>Price</p>
+          <input type="number" name="price" ref={priceRef} />
         </div>
         <div className="inputBtn">
           <p>Category</p>
         </div>
-        <div className="inputBtn">
+        <div className="inputBtn" onClick={submitHandler}>
           <p>Send</p>
         </div>
       </div>
--- src/components/feedbar/Feeds.js
+++ src/components/feedbar/Feeds.js
@@ -1,28 +1,28 @@
 import React from "react";
 import Feed from "./Feed";
-import Avatar from '@mui/material/Avatar';
+import Avatar from "@mui/material/Avatar";
 
 function Feeds() {
   return (
     <div className="border-2 w-3/4  m-auto rounded-xl ">
-      
-      
-    <div className="border w-3/4 m-auto">
-      
-    <div className="border-2 mt-3 flex"><Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg " />
-    <div className="w-1/2 border text-center m-auto">Name</div>
-    </div>
-  </div>
-      
-  <div className="border w-3/4 m-auto mt-5">
-  <Feed />
-  <Feed />
-  <Feed />
-  <Feed />
-  <Feed />
-  <Feed />
-  <Feed />
-  </div>
+      <div className="border w-3/4 m-auto">
+        <div className="border-2 mt-3 flex">
+          <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg " />
+          <div className="w-1/2 border text-left m-auto ml-2">
+            Ivan Veselinov
+          </div>
+        </div>
+      </div>
+
+      <div className="border w-3/4 m-auto mt-5">
+        <Feed />
+        <Feed />
+        <Feed />
+        <Feed />
+        <Feed />
+        <Feed />
+        <Feed />
+      </div>
 
       <div className="flex justify-between w-full pt-3 border-t-2 mt-4 border-b1">
         <div className="inputBtn">
@@ -34,18 +34,16 @@ function Feeds() {
         <div className="inputBtn">
           <p>Add to basket</p>
         </div>
-      
       </div>
-     <div className="border flex justify-between w-full">
-     <div className="inputBtn">
+      <div className="border flex justify-between w-full">
+        <div className="inputBtn">
           <p>Like</p>
         </div>
         <div className="inputBtn">
           <p>Comment</p>
         </div>
+      </div>
     </div>
-  </div>
-    
   );
 }
 
