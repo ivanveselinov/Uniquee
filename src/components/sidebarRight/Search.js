ea32c1fea3dbb578179d3980bfc8f9a8871c52d1
Ivan <ivanveselinov22@gmail.com>
2021-11-17 14:05:40 +1100

    right side done

--- src/components/sidebarRight/Search.js
+++ src/components/sidebarRight/Search.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Search() {
     return (
         <div>
-            <div className="text-2xl text-center mt-10">Search</div>
+            <div className="text-2xl text-center mt-10 ">Search</div>
             <div className="space-x-3 space mt-12 flex-col justify-center ">
             <input type="text" 
             className=" text-gray-700 placeholder-gray-400 font-bold justify-center rounded-full " 
